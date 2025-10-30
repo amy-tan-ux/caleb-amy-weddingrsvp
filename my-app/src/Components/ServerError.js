2503ad37319952f9151248980eb1af24c39f1d51
Amy Tan <amy.tan.su@gmail.com>
2025-10-30 03:03:06 -0400

    updated server down view

--- my-app/src/Components/ServerError.js
+++ my-app/src/Components/ServerError.js
@@ -4,7 +4,7 @@ const ServerError = () => {
         <div className="wedding-container">
           <h1 className="rose-gold-title">Server Error Encountered</h1>
           <p className="story-text">
-            Thank you for taking your time to RSVP to our wedding. Unfortunately our servers are currently down. Please try again later. 
+            Thank you for taking your time to RSVP to our wedding. We are currently unable to reach our servers. Please check your internet connection, refresh and try again later. 
           </p>
         </div>
       );
